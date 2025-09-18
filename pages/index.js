d8cfc92d87732ebb83861822ccac94bfcc72a0d4
João Pedro Panza Mainieri <137027451+Jp-mainieri@users.noreply.github.com>
2025-09-18 19:25:09 +0000

    adiciona arquivo `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Luana, Eu te amo. Se você me ama, da uma risada ae!</h1>;
+  return <h1>Luana, Eu te amo. Se você me ama, da uma risada ae!</h1>;
 }
 
 export default Home;
