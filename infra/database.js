6d279a998e17be00ebb1086002e142709bfa8f82
João Pedro Panza Mainieri <137027451+Jp-mainieri@users.noreply.github.com>
2025-12-06 21:25:46 +0000

    makes database.js client.connect() more robust to erros

--- infra/database.js
+++ infra/database.js
@@ -9,13 +9,24 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     ssl: process.env.NODE_ENV === 'development' ? false : true,
   });
-  await client.connect();
-
+  
+  console.log("Credenciais do postgres: ", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === 'development' ? false : true,
+    
+  })
+  
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
