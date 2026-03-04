cd597d0be86d5f95fc817decb64cda6433d19b88
João Pedro Panza Mainieri <137027451+Jp-mainieri@users.noreply.github.com>
2026-03-04 03:01:06 +0000

    avancando no endpoint /migrations

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,30 +1,31 @@
-import migrationRunner from 'node-pg-migrate'
-import {join} from "node:path"
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+
+const defaultMigrationOptions = {
+  databaseUrl: process.env.DATABASE_URL,
+  dryRun: true,
+  dir: join("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
 
 export default async function migrations(request, response) {
-  if(request.method == "GET") {
-const migrations = await migrationRunner({
-    databaseUrl: process.env.DATABASE_URL,
-    dryRun: false,
-    dir: join("infra","migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  })
-  response.status(200).json(migrations);  }
-  if(request.method === "POST") {
-const migrations = await migrationRunner({
-    databaseUrl: process.env.DATABASE_URL,
-    dryRun: false,
-    dir: join("infra","migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  })
-  response.status(200).json(migrations);  
-}
+  if (request.method == "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    response.status(200).json(pendingMigrations);
+  }
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
 
-response.status(405).end();
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+    response.status(200).json(migratedMigrations);
+  }
 
-  
+  response.status(405).end();
 }
